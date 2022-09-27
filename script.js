a00a7d7b54e4aa597f9e4e638878470d5fc26cd7
Julian Raphael Festing <julfesting@gmail.com>
2022-09-27 20:36:09 +0100

    Added a function to add new list items when an on-click event happens on the add-to-list button

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 /* Exercise 1: Wish list */
+function addToList(item) {
+    $('#items').append('<li>' + item + '</li>')
+
+}
+
+$(document).on('click', '#add-to-list', function() {
+ let form_input = $('#item').val();
+    addToList(form_input);
+    $('#item').focus();
+    $('#item').val('');
+});
