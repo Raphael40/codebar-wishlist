a43cb212577ee173bf202f8b44f7d1b1e962cfdf
Julian Raphael Festing <julfesting@gmail.com>
2022-09-28 15:47:19 +0100

    A new function that adds a done label when the pending button is clicked on

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Exercise 1: Wish list */
 function addToList(item) {
-    $('#items').append('<li>' + item + '</li>')
+    $('#items').append('<li>' + item + '<span class="label pending">Pending</span>' + '</li>')
 
 }
 
@@ -8,5 +8,13 @@ $(document).on('click', '#add-to-list', function() {
  let form_input = $('#item').val();
     addToList(form_input);
     $('#item').focus();
-    $('#item').val('');
+    $('#item').val('');    
 });
+
+$(document).on('click', '.pending', function() {
+    let li_node = $(this).parent();
+    li_node.append("<span class='label success'>Done!</span>");
+    li_node.remove('.pending');
+
+});
+
